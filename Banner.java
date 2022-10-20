184b60f2ec6ac8f8e1a72bcb9b6feb746a33005c
eelizabethriley <eeriley@smith.edu>
2022-10-20 11:10:44 -0400

    Fixed banner length; now adjusts to message length

--- Banner.java
+++ Banner.java
@@ -8,7 +8,7 @@ public class Banner {
     
     /* Message to print on Banner */
     private String message;
-    private int mLength = message.length();
+    private int mLength = 2;
 
     /* Constructor */
     public Banner(String m) {
@@ -19,13 +19,13 @@ public class Banner {
      * TODO: Modify this method to print a decorative banner, resized to fit the message
      */
     public void display() {
-        System.out.println("#");
-        System.out.print("###");
-        System.out.print("-".repeat(mLength+2));
+        System.out.println("    " + "#".repeat(message.length()));
+        System.out.print("  ###");
+        System.out.print("-".repeat(message.length()-2));
         System.out.println("###");
-        System.out.print("#" + this.message + "#");
-        System.out.println("###" + "-" + "###");
-        System.out.println("#");
+        System.out.println(" ## " + this.message + " ##");
+        System.out.println("  ###" + "-".repeat(message.length()-2) + "###");
+        System.out.println("    " + "#".repeat(message.length()));
     }
 
     /* main method (for testing) */
